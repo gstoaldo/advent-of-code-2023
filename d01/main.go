22b34e17ac8881f4946a35f0905b4820a7c753ad
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2023-12-01 12:19:18 -0300

    d01 p2

--- d01/main.go
+++ d01/main.go
@@ -10,11 +10,36 @@ import (
 
 type inputT []string
 
-func calibrationValue(line string) int {
+var mapToDigit = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+	"1":     "1",
+	"2":     "2",
+	"3":     "3",
+	"4":     "4",
+	"5":     "5",
+	"6":     "6",
+	"7":     "7",
+	"8":     "8",
+	"9":     "9",
+}
+
+func calibrationValueP1(line string) int {
 	re := regexp.MustCompile(`\d`)
 
 	matches := re.FindAllString(line, -1)
 	numberStr := ""
+
+	if len(matches) == 0 {
+		return 0
+	}
 
 	if len(matches) == 1 {
 		numberStr = matches[0] + matches[0]
@@ -27,11 +52,74 @@ func calibrationValue(line string) int {
 	return number
 }
 
+func calibrationValueP2(line string) int {
+	first := ""
+	second := ""
+
+	// find first digit
+	for i := range line {
+		for key, value := range mapToDigit {
+			if i+len(key) > len(line) {
+				continue
+			}
+
+			substring := line[i : i+len(key)]
+
+			if substring == key {
+				first = value
+				break
+			}
+		}
+
+		if first != "" {
+			break
+		}
+	}
+
+	// find second digit
+	for i := len(line); i > 0; i-- {
+		for key, value := range mapToDigit {
+			if i-len(key) < 0 {
+				continue
+			}
+
+			substring := line[i-len(key) : i]
+
+			if substring == key {
+				second = value
+				break
+			}
+		}
+
+		if second != "" {
+			break
+		}
+	}
+
+	//
+	numberStr := first + second
+	number, _ := strconv.Atoi(numberStr)
+
+	return number
+}
+
 func part1(input inputT) int {
 	sum := 0
 
 	for _, line := range input {
-		sum += calibrationValue(line)
+		sum += calibrationValueP1(line)
+	}
+
+	fmt.Println(sum)
+
+	return sum
+}
+
+func part2(input inputT) int {
+	sum := 0
+
+	for _, line := range input {
+		sum += calibrationValueP2(line)
 	}
 
 	fmt.Println(sum)
@@ -40,7 +128,8 @@ func part1(input inputT) int {
 }
 
 func main() {
-	input := utils.ReadLines("d01/input.txt")
+	input := utils.ReadLines("input.txt")
 
 	part1(input)
+	part2(input)
 }
--- d01/main_test.go
+++ d01/main_test.go
@@ -6,21 +6,55 @@ import (
 	"github.com/gstoaldo/advent-of-code-2023/utils"
 )
 
-var example = utils.ReadLines("example.txt")
+var example1 = utils.ReadLines("example1.txt")
+var example2 = utils.ReadLines("example2.txt")
 
-func TestCalibrationValue(t *testing.T) {
+func TestCalibrationValueP1(t *testing.T) {
 	testcases := []struct {
 		line     string
 		expected int
 	}{
-		{example[0], 12},
-		{example[1], 38},
-		{example[2], 15},
-		{example[3], 77},
+		{example1[0], 12},
+		{example1[1], 38},
+		{example1[2], 15},
+		{example1[3], 77},
 	}
 
 	for _, tc := range testcases {
-		got := calibrationValue(tc.line)
+		got := calibrationValueP1(tc.line)
+
+		if got != tc.expected {
+			t.Fatalf("expected: %v, got: %v", tc.expected, got)
+		}
+	}
+}
+
+func TestCalibrationValueP2(t *testing.T) {
+	testcases := []struct {
+		line     string
+		expected int
+	}{
+		{example2[0], 29},
+		{example2[1], 83},
+		{example2[2], 13},
+		{example2[3], 24},
+		{example2[4], 42},
+		{example2[5], 14},
+		{example2[6], 76},
+		{"rphtbkncs4nznsix", 46},
+		{"nineeighttworhtvxdtxp8twoneh", 91},
+		{"63eightsixgdsdqqxzzsbnkt782", 62},
+		{"three6fivefoursixgtzfzbkhmnplfm", 36},
+		{"fourgtwopbjbcvgtwo3one", 41},
+		{"5three1", 51},
+		{"nine671seventwotwonejkf", 91},
+		{"twone", 21},
+		{"21", 21},
+		{"shrzvdcghblt21", 21},
+	}
+
+	for _, tc := range testcases {
+		got := calibrationValueP2(tc.line)
 
 		if got != tc.expected {
 			t.Fatalf("expected: %v, got: %v", tc.expected, got)
