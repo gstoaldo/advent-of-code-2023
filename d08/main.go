929ef09b8c0f5763ca651d1f43f73ee985af3553
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2023-12-08 11:06:26 -0300

    d08 p2

--- d08/main.go
+++ d08/main.go
@@ -29,21 +29,65 @@ func parse(filepath string) (string, nodesT) {
 	return instructions, nodes
 }
 
+func next(current string, nsteps int, instructions string, nodes nodesT) string {
+	direction := string(instructions[nsteps%len(instructions)])
+	return nodes[current][lrToIndex[direction]]
+}
+
 func part1(instructions string, nodes nodesT) int {
 	nsteps := 0
 	current := "AAA"
 
 	for current != "ZZZ" {
-		direction := string(instructions[nsteps%len(instructions)])
-		current = nodes[current][lrToIndex[direction]]
-
+		current = next(current, nsteps, instructions, nodes)
 		nsteps++
 	}
 
 	return nsteps
 }
 
+func startList(nodes nodesT) []string {
+	result := []string{}
+	for n := range nodes {
+		if string(n[2]) == "A" {
+			result = append(result, n)
+		}
+	}
+	return result
+}
+
+func isExit(node string) bool {
+	return node[2] == 'Z'
+}
+
+func calcFrequency(node string, instructions string, nodes nodesT) int {
+	nsteps := 0
+	current := node
+
+	for nsteps < 50_000 {
+		current = next(current, nsteps, instructions, nodes)
+
+		if isExit(current) {
+			return nsteps + 1
+		}
+
+		nsteps++
+	}
+
+	panic(fmt.Sprintf("frequency not found for node %v", node))
+}
+
+func part2(instructions string, nodes nodesT) []int {
+	multiples := []int{}
+	for _, c := range startList(nodes) {
+		multiples = append(multiples, calcFrequency(c, instructions, nodes))
+	}
+
+	return multiples
+}
+
 func main() {
 	instructions, nodes := parse(utils.Filepath())
 	fmt.Println(part1(instructions, nodes))
+	fmt.Println(part2(instructions, nodes)) // TODO: calculate least commom multiple
 }
