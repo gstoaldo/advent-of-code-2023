71401f6c3a32827bccd9fb2a0805c83dc2a75d09
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2023-12-15 10:17:28 -0300

    d15 p2

--- d15/main.go
+++ d15/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/gstoaldo/advent-of-code-2023/utils"
@@ -23,6 +25,85 @@ func hash(instruction string) int {
 	return result
 }
 
+type lensT struct {
+	label       string
+	focalLength int
+}
+
+func remove(hashmap map[int][]lensT, boxID int, lens lensT) {
+	boxContent := hashmap[boxID]
+
+	newContent := []lensT{}
+
+	for _, l := range boxContent {
+		if l.label != lens.label {
+			newContent = append(newContent, l)
+		}
+	}
+
+	hashmap[boxID] = newContent
+}
+
+func addOrReplace(hashmap map[int][]lensT, boxID int, lens lensT) {
+	boxContent := hashmap[boxID]
+
+	newContent := []lensT{}
+	found := false
+
+	for _, l := range boxContent {
+		if l.label == lens.label {
+			newContent = append(newContent, lens)
+			found = true
+		} else {
+			newContent = append(newContent, l)
+		}
+	}
+
+	if !found {
+		newContent = append(newContent, lens)
+	}
+
+	hashmap[boxID] = newContent
+}
+
+func getHashmap(instructions []string) map[int][]lensT {
+	hashmap := map[int][]lensT{}
+
+	re := regexp.MustCompile(`(\w+)([=|-])(\d)?`)
+
+	for _, instruction := range instructions {
+		groups := re.FindStringSubmatch(instruction)
+
+		boxID := hash(groups[1])
+		lens := lensT{label: groups[1]}
+		operation := groups[2]
+
+		if operation == "-" {
+			remove(hashmap, boxID, lens)
+		}
+
+		if operation == "=" {
+			lens.focalLength, _ = strconv.Atoi(groups[3])
+			addOrReplace(hashmap, boxID, lens)
+		}
+	}
+
+	return hashmap
+}
+
+func focusingPower(hashmap map[int][]lensT) int {
+	result := 0
+
+	for boxID, content := range hashmap {
+		for i, l := range content {
+			result += (boxID + 1) * (i + 1) * l.focalLength
+		}
+	}
+
+	return result
+
+}
+
 func part1(instructions []string) int {
 	result := 0
 
@@ -33,7 +114,12 @@ func part1(instructions []string) int {
 	return result
 }
 
+func part2(instructions []string) int {
+	return focusingPower(getHashmap(instructions))
+}
+
 func main() {
 	instructions := parse(utils.Filepath())
 	fmt.Println(part1(instructions))
+	fmt.Println(part2(instructions))
 }
